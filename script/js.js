80e2f294a2a919fc41f6ce966252ca49a617a863
Elias Dottori <elias.dottori@hotmail.com>
2022-11-27 15:56:58 -0300

    ver pre-final

--- script/js.js
+++ script/js.js
@@ -1,20 +1,20 @@
-
 const toggleButton = document.getElementById("button-menu");
 const navWrapper = document.getElementById("nav");
 const navUL = document.getElementById("navBarUL");
-const letter = document.querySelectorAll(".letter")
-
+const letter = document.querySelectorAll(".letter");
+const nameNavBar = document.getElementById("nameNavBar");
 
 toggleButton.addEventListener("click", () => {
   navUL.classList.toggle("navUlResponsive");
   navWrapper.classList.toggle("navResponsive");
-  letter.forEach(x => x.classList.toggle("navUlResponsive"))
+  nameNavBar.classList.toggle("nameResponsive");
+  letter.forEach((x) => x.classList.toggle("navUlResponsive"));
 });
-
 
 navWrapper.addEventListener("click", (e) => {
   if (e.target.id === "nav") {
     navUL.classList.remove("navUlResponsive");
     navWrapper.classList.remove("navResponsive");
+    letter.forEach((x) => x.classList.remove("navUlResponsive"));
   }
 });
